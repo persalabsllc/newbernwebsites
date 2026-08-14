a2d9bcc1d2d20512ddb8d041806580826e0e65f4
persalabsllc <persalabsllc@gmail.com>
2026-08-14 11:27:34 -0400

    Add admin CRM route

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import AdminCRM from '../../components/AdminCRM';
+
+export const metadata = {
+  title: 'New Bern Websites | CRM',
+  robots: { index: false, follow: false },
+};
+
+export default function AdminPage() {
+  return <AdminCRM />;
+}
